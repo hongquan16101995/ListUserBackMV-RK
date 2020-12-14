20d3e7b9332fa98ecc1c0ca2b0c4ddd8b867d507
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-12-14 08:19:47 +0700

    fullop để lưu trữ

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
